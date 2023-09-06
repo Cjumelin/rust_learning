d58aa1ad42382eb17a9ed57a2cdb16a5e6c5ba86
cjumelin <cjumelin@protonmail.com>
2023-09-06 14:44:26 -0400

    init

---
+++ guessing_games/src/main.rs
@@ -0,0 +1,34 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let number_to_guess = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Guess input:");
+        
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        match guess.cmp(&number_to_guess) {
+            Ordering::Less => println!("+"),
+            Ordering::Greater => println!("-"),
+            Ordering::Equal => {
+                println!("Success");
+                break;
+            }
+        }
+    }
+
+}
---
+++ range/src/main.rs
@@ -0,0 +1,7 @@
+fn main()-> i32 {
+    let range = 1..11;
+    for i in range {   
+        println!("{}", i);
+    }
+    range
+}
